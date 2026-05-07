1c22c4b67048bd993f69398ba0409c4be8b7645b
Ibrahim Balta <ibrahim@example.com>
2026-05-07 19:14:13 +0300

    chore: use default tanstack server for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [
     tanstackStart({
-      server: { entry: "src/server.ts" },
       deployment: "vercel",
     }),
     react(),
